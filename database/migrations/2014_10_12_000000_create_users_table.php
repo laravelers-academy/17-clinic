9c68c1e7b3344b95f4b9dc4397eb7f492d00da37
Homero Raul <h.raulvc@gmail.com>
2020-04-02 21:22:25 -0600

    Verificación de Email y make:auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
+            // Se emplea para saber si una cuenta ha sido verificada
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
