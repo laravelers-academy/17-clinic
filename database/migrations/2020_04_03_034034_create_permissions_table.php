54b97dd8eaeb61085ff5e3daf41e0afcef894652
Homero Raul <h.raulvc@gmail.com>
2020-04-02 21:42:58 -0600

    Trabajao de migración para los roles

--- database/migrations/2020_04_03_034034_create_permissions_table.php
+++ database/migrations/2020_04_03_034034_create_permissions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRolesTable extends Migration
+class CreatePermissionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,8 @@ class CreateRolesTable extends Migration
      */
     public function up()
     {
-        Schema::create('roles', function (Blueprint $table) {
+        Schema::create('permissions', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->stiing('slug');
-            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
@@ -29,6 +26,6 @@ class CreateRolesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('roles');
+        Schema::dropIfExists('permissions');
     }
 }
